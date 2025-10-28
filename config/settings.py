238373ffa121f82d07991d490c91f6fc61e48c34
Lucas C. Pires <lcpires7.dev@gmail.com>
2025-10-28 13:46:20 -0300

    upload

--- config/settings.py
+++ config/settings.py
@@ -142,13 +142,7 @@ REST_FRAMEWORK = {
 # =========================
 # CORS
 # =========================
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8000',
-    'http://localhost:5173'
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
