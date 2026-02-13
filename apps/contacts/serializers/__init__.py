5125f96260634576bb9d5230cf6c7e08fde3e14b
Lucas Pires <lcpires7.dev@gmail.com>
2026-02-12 22:08:49 -0300

    update contact

--- apps/contacts/serializers/__init__.py
+++ apps/contacts/serializers/__init__.py
@@ -2,6 +2,7 @@
 from .contact_serializer import ContatoSerializer, ContatoCreateSerializer
 from .guardian_serializers import GuardianDetailSerializer, StudentSummarySerializer
 from .serializers import WhatsAppContactSerializer
+from .guardian_unified_serializer import GuardianUnifiedSerializer  # ✨ NOVO
 
 __all__ = [
     'ContatoSerializer',
@@ -9,4 +10,5 @@ __all__ = [
     'GuardianDetailSerializer',
     'StudentSummarySerializer',
     'WhatsAppContactSerializer',
+    'GuardianUnifiedSerializer',  # ✨ NOVO
 ]
